cd2b15881299b0618ca0edd671b26518f8244851
Amey Prakash <aditiaditi737@gmail.com>
2026-01-29 17:55:11 +0530

    Added number pattern 1

---
+++ pattern1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n, i = 1;
+    cout << " Enter number of rows: ";
+    cin >> n;
+    while (i <= n) {
+        int j = 1;
+        while(j <= n){
+            cout << j << " ";
+            j++;
+        }
+        cout << endl;
+        i++;
+    
+    }
+    return 0;
+}
