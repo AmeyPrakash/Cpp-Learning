f02a03dd387b97b2cf5ff5813e2f3f522f3100b3
Amey Prakash <aditiaditi737@gmail.com>
2026-01-31 19:01:57 +0530

    Pattern 7

---
+++ Pattern Printing/pattern7.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n, i, j;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    i = 1;
+    while (i <= n)
+    {
+        j = 1;
+        while(j <= i) {
+            cout << i - j + 1 << " ";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+    
+
+    return 0;
+}
