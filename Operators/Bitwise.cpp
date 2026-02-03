d58576d016bb1028ce9dfb2cb69648c54ee7ab17
Amey Prakash <aditiaditi737@gmail.com>
2026-02-03 12:16:24 +0530

    Bitwise operators

---
+++ Operators/Bitwise.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a = 5;
+    int b = 6;
+    cout << "Bitwise AND (a & b): " << (a & b) << endl; 
+    cout << "Bitwise OR (a | b): " << (a | b) << endl;    
+    cout << "Bitwise XOR (a ^ b): " << (a ^ b) << endl;  
+    cout << "Bitwise NOT (~a): " << (~a) << endl;      
+    cout << "Left Shift (a << 1): " << (a << 1) << endl; 
+    cout << "Right Shift (b >> 1): " << (b >> 1) << endl; 
+    return 0;
+}
