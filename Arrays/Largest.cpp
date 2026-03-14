60767db13d0b08a9a02963a70529b1145da50fd8
Amey Prakash <aditiaditi737@gmail.com>
2026-03-14 18:24:50 +0530

    index of the largest no. in array

--- Arrays/Largest.cpp
+++ Arrays/Largest.cpp
@@ -3,14 +3,19 @@ using namespace std;
 #include <climits>
 
 int main(){
-    
+    int index = 0;
     int size = 6;
     int largest = INT_MIN;
     int arr[size] = {5,15,22,1,-15,24};
     for (int i = 0; i < size; i++)
     {
-        largest = max(arr[i], largest);
-    }    
+         if (arr[i] > largest)
+        {
+            largest = arr[i];
+            index = i;
+        }
+    }
+    cout << index << endl;
     cout << largest << endl;
     return 0;
 }
