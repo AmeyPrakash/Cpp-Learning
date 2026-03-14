43affd6b31184006ef3c126d11bb1b23339f4c33
Amey Prakash <aditiaditi737@gmail.com>
2026-03-14 17:48:01 +0530

    smallest and kargest in an array

---
+++ Arrays/Largest.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int size = 6;
+    int largest = INT_MIN;
+    int arr[size] = {5,15,22,1,-15,24};
+    for (int i = 0; i < size; i++)
+    {
+        largest = max(arr[i], largest);
+    }    
+    cout << largest << endl;
+    return 0;
+}
---
+++ Arrays/Smallest.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int size = 6;
+    int smallest = INT_MAX;
+    int arr[size] = {5,15,22,1,-15,24};
+    for (int i = 0; i < size; i++)
+    {
+        // if (arr[i] < smallest)
+        // {
+        //     smallest = arr[i];
+        // }
+        smallest = min(arr[i], smallest);
+    }
+    cout << smallest << endl;
+    
+
+
+    return 0;
+}
