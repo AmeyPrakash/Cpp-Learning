7b51cbe0404bc912d0154d772ddd86f7f8ccc18c
Amey Prakash <aditiaditi737@gmail.com>
2026-03-17 23:31:09 +0530

    time and space complexity

---
+++ Extra/TIme Complexity.cpp
@@ -0,0 +1,31 @@
+/*Not the actual time taken but amount of time taken as function  of input size*/
+
+/*
+Big o Notation (symbol) worst case (upper bound)
+
+f(x)=4n^2+3n+5
+f(x)=n^2+n+1
+f(X)=n^2
+
+O(n^2)
+
+Theta (average case)
+
+best (omega)(lower bound)
+
+
+SPACE COMPLEXITY
+
+amount of space taken by an algorithm as function of input size (n)
+
+input array =x = n does not count 
+
+input array = x square new creation will be counted therefore O(n)
+
+
+
+
+O(1),O(log n),O(n),O(n log n)
+O(n^2),O(n^3),O(2^n),O(n!).
+
+*/
--- Vector/singlenumber.cpp
+++ Vector/singlenumber.cpp
@@ -3,7 +3,8 @@ using namespace std;
 #include<vector>
  
 int main(){
-    vector<int> vec={4,1,2,1,2,};
+    vector<int> vec={4,1,2,1,2,};     //n^n=0
+                                      //n^0=n
     int ans=0;
     for(int i: vec)
     {
