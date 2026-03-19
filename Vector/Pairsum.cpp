259383dbb518975cf8321f0cc98f5998e7631f70
Amey Prakash <aditiaditi737@gmail.com>
2026-03-19 19:31:36 +0530

    pair sum two pointer approach

--- Vector/Pairsum.cpp
+++ Vector/Pairsum.cpp
@@ -32,3 +32,35 @@ int main(){
     return 0;
 } 
     */
+vector<int> pairsum(vector<int> nums,int target){
+    vector<int> ans;
+    int n = nums.size();
+    int i = 0, j = n-1;
+
+    while (i<j)
+    {
+        int pairsum = nums[i]+nums[j];
+        if(pairsum<target){
+            i++;
+        }
+        else if (pairsum>target)
+        {
+            j--;
+        }
+        else{
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;
+        }
+    }
+    return ans;
+    
+}
+
+int main(){
+    vector<int> nums={2,7,11,15};
+    int target = 9;
+    vector<int> ans = pairsum(nums,target);
+    cout<< ans[0] <<" " << ans[1] << endl;
+    return 0;
+}
