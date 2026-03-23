18e3b3e6266a5b57c5d335cf7c2fb73360f6221e
Amey Prakash <aditiaditi737@gmail.com>
2026-03-23 18:13:56 +0530

    List

--- Extra/STL.cpp
+++ Extra/STL.cpp
@@ -1,15 +1,35 @@
 #include<iostream>
 using namespace std;
 #include<vector>
+#include<list>
 
 int main(){
-    vector<int> vec={1,2,3,4,5};
+
+    //VECTOR ITERATORS
+    // vector<int> vec={1,2,3,4,5};
     
 
-    for (auto it=vec.rbegin();it!=vec.rend();it++)
-    {
-        cout<<*(it);
+    // for (auto it=vec.rbegin();it!=vec.rend();it++)
+    // {
+    //     cout<<*(it);
+    // }
+    list<int> l;
+    l.emplace_back(1);
+    l.push_back(2);
+    l.push_front(3);
+    l.push_front(4);
+    l.pop_back();
+    l.pop_front();
+    for (int i : l){
+        cout<< i <<endl;
+
     }
+    cout<<endl;
+
+    
+
+    
+
     
     return 0;
 }
