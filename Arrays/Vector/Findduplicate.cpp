16fb4b1c5fe10ce870aaded7324aa09679d1d1f3
Amey Prakash <aditiaditi737@gmail.com>
2026-03-28 17:27:23 +0530

    Duplicate in an array

---
+++ Arrays/Vector/Findduplicate.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#include<unordered_set>
+
+// HASHING  
+
+// int duplicate(vector<int>& nums){
+//     unordered_set<int> s;
+//     for(int val: nums){
+//         if(s.find(val)!=s.end()){
+//             return val;
+
+//         }
+//         s.insert(val);
+//     }
+//     return -1;
+
+// }
+
+
+// Slow fast Pointer approach (Linked list)
+//two node will point to one
+int duplicate(vector<int>& nums){
+    int slow = nums[0],fast=nums[0];
+    do{
+        slow=nums[slow];//+1
+        fast=nums[nums[fast]]; //+2
+    }while(slow!=fast);
+
+    slow=nums[0];
+
+    while (slow!=fast)
+    {
+        slow = nums[slow];//+1
+        fast = nums[fast];//+1
+    }
+    return slow;
+}
+
+int main(){
+    vector<int> y = {3,1,3,4,2};
+    cout<< duplicate(y) <<endl;
+   
+    
+
+    return 0;
+}
