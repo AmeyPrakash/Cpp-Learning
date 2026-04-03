aecd647bb851b2bea1b469a725a18441b371be56
Amey Prakash <aditiaditi737@gmail.com>
2026-04-03 23:32:26 +0530

    output in list

--- Arrays/Vector/tripletsumzero.cpp
+++ Arrays/Vector/tripletsumzero.cpp
@@ -2,9 +2,9 @@
 #include<vector>
 #include<algorithm>
 
-std::vector<int> tripletsum(std::vector<int>& nums, int target){
+std::vector<std::vector<int>> tripletsum(std::vector<int>& nums, int target){
     int n = nums.size();
-    std::vector<int> ans;
+    std::vector<std::vector<int>> ans;
     std::sort(nums.begin(),nums.end());   //Sorted the array
 
 
@@ -19,9 +19,7 @@ std::vector<int> tripletsum(std::vector<int>& nums, int target){
 
             if (s==sum){
             
-                ans.push_back(nums[i]);
-                ans.push_back(nums[left]);
-                ans.push_back(nums[right]);
+                ans.push_back({nums[i], nums[left], nums[right]});
                 left++;
                 right--;
                 while (left<n && nums[left]==nums[left-1])   //To avoid duplicates
@@ -52,9 +50,12 @@ std::vector<int> tripletsum(std::vector<int>& nums, int target){
 int main(){
     std::vector<int> nums = {-1,0,1,2,-1,-4};
     int target = 0;
-    std::vector<int> ans = tripletsum(nums,target);
+    std::vector<std::vector<int>> ans = tripletsum(nums,target);
     for(int i=0;i<ans.size();i++){
-        std::cout<<ans[i]<<" ";
+        for(int j=0;j<ans[i].size();j++){
+            std::cout<<ans[i][j]<<" ";
+        }
+        std::cout<<std::endl;
     }
     return 0;
 }
