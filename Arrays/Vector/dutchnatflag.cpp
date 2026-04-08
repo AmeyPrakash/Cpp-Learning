7e4f28f615c5857d8075d2f00edf80bb205c6a83
Amey Prakash <aditiaditi737@gmail.com>
2026-04-08 22:16:57 +0530

    dutch national flag arrange colors which represents 0,1and2.

---
+++ Arrays/Vector/dutchnatflag.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#include<algorithm>
+
+
+void sortColors(vector<int>& nums) {
+    int n = nums.size();
+    int high = n-1, low = 0, mid = 0;
+    while(mid <= high){
+        if(nums[mid]==0){
+            swap(nums[low], nums[mid]);
+            low++;
+            mid++;
+
+        }
+        else if(nums[mid]==1){
+            mid++;
+        }
+        else{
+            swap(nums[mid], nums[high]);
+            high--;
+        }
+    }
+        
+}
+int main(){
+    vector<int> nums = {2,0,2,1,1,0};
+    sortColors(nums);
+    for(int i: nums){
+        cout<<i<<" ";
+    }
+}
