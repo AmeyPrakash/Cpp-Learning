0cfe4f0d7f946adfb08d677b8b4ed2dac8474e43
AmeyPrakash <aditiaditi737@gmail.com>
2026-05-10 00:17:26 +0530

    single element

---
+++ Arrays/Vector/Basics/singleno.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans =0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            ans = ans ^ nums[i];
+        }
+        return ans;
+
+        
+    }
+};
+int main(){
+    Solution s;
+    vector<int> nums = {2,2,1};
+    cout << s.singleNumber(nums) << endl;
+    return 0;
+}
