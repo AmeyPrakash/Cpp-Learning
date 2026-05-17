67436c87be98c9ca803bc29dfcd2354579d4aaf8
AmeyPrakash <aditiaditi737@gmail.com>
2026-05-17 16:20:24 +0530

    move zeroes

---
+++ Vector/Two Pointers/movezeroes.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+        for(int j = 0; j <n; j++){
+            if(nums[j] != 0){
+                nums[i] = nums [j];
+                nums[j] = 0;
+                i++;
+            }
+        }
+
+        
+    }
+};
+int main(){
+    Solution s;
+    vector<int> arr = {0,1,0,3,1,2};
+    s.moveZeroes(arr);
+    for(auto x:arr){
+        cout<<x<<endl;
+    }
+}
