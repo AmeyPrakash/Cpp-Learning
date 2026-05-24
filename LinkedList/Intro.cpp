87cf4de0bec9e8903d0dcfb2c38758cf72dda404
AmeyPrakash <aditiaditi737@gmail.com>
2026-05-24 09:32:13 +0530

    pop_back, pop_front, insert, search in ll

--- LinkedList/Intro.cpp
+++ LinkedList/Intro.cpp
@@ -21,6 +21,7 @@ class List
         head = NULL;
         tail = NULL;       
     }
+
     void push_front(int val){
         Node* newNode = new Node(val); 
         if(head == NULL){
@@ -32,6 +33,7 @@ class List
             head = newNode;
         }
     }
+
     void push_back(int val){
         Node* newNode = new Node(val);
         if(head == NULL){
@@ -42,6 +44,69 @@ class List
             tail->next = newNode;
             tail = newNode;
         }
+    }
+
+    void pop_front(){
+        if(head == NULL){
+            return;
+        }
+        
+            Node* temp = head;
+            head = head->next;
+            temp->next = NULL;
+            delete temp;
+
+    }
+
+    void pop_back(){
+        if(head == NULL){
+            return;
+        }
+        Node* temp = head;
+        while(temp->next != tail){
+            temp = temp->next;
+        }
+        temp->next = NULL;
+        delete tail;  //memory deleted
+        tail = temp;
+    }
+    void insert(int val, int pos){
+        if(pos<0){
+            cout<<"invalid Position\n";
+            return;
+        }
+        if(pos == 0){
+            push_front(val);
+            return;
+        }
+        Node* temp = head;
+        for (int i = 0; i < pos - 1; i++)
+        {
+            temp = temp->next;
+        }
+        Node* newNode = new Node(val);
+        newNode->next = temp->next;
+        temp->next = newNode;
+        
+    }
+    void search(int key){
+        Node* temp = head;
+        int pos = 0;
+        if(temp == NULL){
+            cout<< "list if empty\n";
+            return;
+        }
+        while (temp != NULL)
+        {
+            if(temp->data == key){
+                cout<<"Found at position: " << pos<<"\n";
+                return;
+            }
+            temp = temp->next;
+            pos++;
+            
+        }
+        
     }
 
     void printLL() {
@@ -61,8 +126,14 @@ int main(){
     List ll;
     ll.push_front(10);
     ll.push_front(20);
+    ll.push_front(50);
+    ll.push_back(40);
     ll.push_back(30);
+    ll.pop_front(); //50 is deleted
+    ll.pop_back();//30 is deleted
+    ll.insert(4,2);
     ll.printLL();
+    ll.search(4);
 
     return 0;
 }
