f7f7d5415e876408d9dff0895969874500c63e4a
AmeyPrakash <aditiaditi737@gmail.com>
2026-06-19 21:23:34 +0530

    next larger element

---
+++ Stack/next_larger.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> nextLargerElement(vector<int>& arr) {
+        int n = arr.size();
+        if(0 == n) return {};
+        vector<int> res(n);
+        stack<int> st;
+        res[n-1] = -1;
+        st.push(arr[n-1]);
+        for(int i = n-2; i >= 0; i--){
+            while(!st.empty() && st.top() <= arr[i]){
+                st.pop();
+            }
+            if(st.empty()){
+                res[i] = -1;
+            }
+            else{
+                res[i] = st.top();
+            }
+            st.push(arr[i]);
+        }
+        return res;
+    }
+};
