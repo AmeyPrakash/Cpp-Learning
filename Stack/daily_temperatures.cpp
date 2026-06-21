2dc46270db995e3c17ba225f4d617b31eb50a171
AmeyPrakash <aditiaditi737@gmail.com>
2026-06-21 20:06:04 +0530

    Daily temperatures

---
+++ Stack/daily_temperatures.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int> st;
+        int n = temperatures.size();
+        vector<int> res(n);
+        res[n-1] = 0;
+        st.push(n-1);
+        for(int i = n-2 ; i>=0; i--){
+            while(!st.empty() && temperatures[st.top()] <= temperatures[i]){
+                st.pop();
+            }
+            if(!st.empty()){
+                res[i] = st.top() - i;
+            }
+            st.push(i);
+        }
+        return res;
+        
+    }
+};
