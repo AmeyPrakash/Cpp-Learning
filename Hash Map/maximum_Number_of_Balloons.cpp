85c066f0b8705bc7d98958e72866c4718b955f13
AmeyPrakash <aditiaditi737@gmail.com>
2026-06-27 20:26:42 +0530

    Maximum Number of Balloons

---
+++ Hash Map/maximum_Number_of_Balloons.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<unordered_map>
+#include<string>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        unordered_map<char, int> have;
+        for(auto i : text){
+            have[i]++;
+        }
+        unordered_map<char, int> need;
+        need['b'] = 1;
+        need['a'] = 1;
+        need['l'] = 2;
+        need['o'] = 2;
+        need['n'] = 1;
+        int res = INT_MAX;
+        for(auto i: need){
+            char c = i.first;
+            int fneed = i.second;
+            int fhave = have[c];
+            int times = fhave / fneed;
+            res = min(res , times);
+        }
+        return res;
+        
+    }
+};
