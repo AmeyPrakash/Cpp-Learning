96b35de4856b3a76588f78a3522513583b8eba2a
AmeyPrakash <aditiaditi737@gmail.com>
2026-07-14 20:19:17 +0530

    Find first palindrome string in array

---
+++ String/Two Pointer/find_first_palindrome_string_in_array.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    string firstPalindrome(vector<string>& words) {
+        
+        for (string word : words) {
+            string rev = word;
+            reverse(rev.begin(), rev.end());
+
+            if (word == rev) {
+                return word;
+            }
+        }
+        return "";
+    }
+};
