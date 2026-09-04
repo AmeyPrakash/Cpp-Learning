de8e3b9237c8debe4eefdacdbf1a228580d8991b
Amey Prakash <aditiaditi737@gmail.com>
2026-09-04 21:10:27 +0530

    smallest stable index I

---
+++ Arrays/smallest_stable_index.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> minindex(n, 0);
+        int minel = INT_MAX;
+        int maxel = INT_MIN;
+        for(int i = n-1; i>=0; i--){
+            minel = min(minel, nums[i]);
+            minindex[i] = minel;
+        }
+        for(int i = 0; i < n; i++){
+            maxel = max(maxel, nums[i]);
+            minel = minindex[i];
+            if(maxel - minel <= k){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
