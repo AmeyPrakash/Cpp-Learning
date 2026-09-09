8c8af29952e13ad73b1c293e436d9a0dafe0a882
Amey Prakash <aditiaditi737@gmail.com>
2026-09-09 18:14:34 +0530

    count commas I

---
+++ Loop/count_commas.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int countCommas(int n) {
+        int count =0; 
+        if(n<1000) return 0;
+
+        return n-999;
+    }
+};
